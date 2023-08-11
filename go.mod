92f8ee679fdee08d63504c0c39ff20051b0094b0
ryota-sato07 <satour.0722@gmail.com>
2023-08-12 08:39:57 +0900

    create user table

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require gopkg.in/ini.v1 v1.67.0
 
-require github.com/stretchr/testify v1.8.4 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+)
--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	/** go.mod ファイルの module 名が、importパス(todo_app_mod) に影響する */
-	"todo_app_mod/config"
+	"todo_app_mod/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+	//
+	// log.Println("test")
 
-	log.Println("test")
+	fmt.Println(models.Db)
 }
