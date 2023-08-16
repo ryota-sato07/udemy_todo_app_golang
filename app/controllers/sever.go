b5bef80c1ae6691b92f282391e54d2ac3f4a8e28
ryota-sato07 <satour.0722@gmail.com>
2023-08-16 18:28:20 +0900

    styled html

--- app/controllers/sever.go
+++ app/controllers/sever.go
@@ -9,6 +9,9 @@ import (
  * サーバー起動
  */
 func StartMainServer() error {
+	files := http.FileServer(http.Dir(config.Config.Static))
+	http.Handle("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
